7ec135d20af60fc93ea77a1ea704e504c5a8b37f
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-03-09 10:41:56 +0530

    Update Conversions.cpp

--- Codes/BASICS/Conversions.cpp
+++ Codes/BASICS/Conversions.cpp
@@ -34,8 +34,13 @@ using namespace std;
 int binaryTodecimal(int n)
 {
     int decimal = 0;
-
-
+    int i = 0;
+    while(n)
+    {
+        int bit = n % 10;
+        decimal = decimal + bit * pow(2,i++);
+        n = n / 10;
+    }
     return decimal;
 }
 
