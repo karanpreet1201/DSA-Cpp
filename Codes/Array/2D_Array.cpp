2be498a160dfb83a88f6a4fff1c37dbb89f742df
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-05-01 14:58:12 +0530

    Update 2D_Array.cpp

--- Codes/Array/2D_Array.cpp
+++ Codes/Array/2D_Array.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <limits.h>
+#include <vector>
 using namespace std;
 
-void printArray(int arr[][3] , int row , int col)
+void printArray(int arr[][4] , int row , int col)
 {
     // For Row Wise print.....
     for (int i = 0 ; i < row ; i++)
@@ -65,8 +66,31 @@ int findMax(int arr[][4] , int row , int col)
 
 void arraySum(int arr[][4] , int row , int col)
 {
-    
+    for (int i = 0 ; i < row ; i++)
+    {
+        int sum = 0;
+        for (int j = 0 ; j < col ; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << "Sum is : " << sum << endl;
+    }
 }
+
+
+// Transpose of a matrix 
+
+void transposeMatrix(int arr[][4] , int row , int col)
+{
+    for(int i = 0 ; i < row ; i++)
+    {
+        for (int j = i ; j < col ; j++)
+        {
+            swap(arr[i][j] , arr[j][i]);
+        }
+    }
+}
+
 
 int main()
 {
@@ -83,18 +107,30 @@ int main()
     // int crr[][] = {{1,2,3,4} , {5,6,7,8} , {9,10,11,12}};
 
 
-    int arr[3][4] = {
-                        {1,2,3,4} , 
-                        {5,6,7,8} , 
-                        {9,10,11,12}
-                    };
-    // int target = 11;
-    int row = 3;
-    int col = 4;
+    // int arr[4][4] = {
+    //                     {100,200,300,400} , 
+    //                     {500,600,700,800} , 
+    //                     {900,110,111,112} ,
+    //                     {113,114,115,116}
+    //                 };
+    // // int target = 11;
+    // int row = 4;
+    // int col = 4;
 
-    arraySum(arr , row , col);
+    // cout << "Array before transpose : " << endl;
+    // printArray(arr , row , col);
 
-    cout << "Maximum number in this array is : " << findMax(arr , row , col) << endl;
+
+
+    // transposeMatrix(arr , row , col);
+
+
+    // cout << "Array after transpose : " << endl;
+    // // arraySum(arr , row , col);
+
+    // cout << "Maximum number in this array is : " << findMax(arr , row , col) << endl;
+    // printArray(arr , row , col);
+
     // int tf = findTarget(arr , row , col , target);
     // if (tf == 1)
     // {
@@ -131,6 +167,48 @@ int main()
 
     // cout << endl;
     // printArray(arr , row , col);
+
+    // 2-D ARRAY IN VECTOR ...
+
+    vector< vector<int> > arr(5 , vector<int>(10,0));
+
+    for (int i = 0 ; i < arr.size() ; i++)
+    {
+        for (int j = 0 ; j < arr[i].size() ; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Jagged Array :- 
+    /* The array whose all the row size are not same 
+    
+    */
+
+   cout << "Jagged array is : " << endl;
+   vector <vector <int> > brr;
+
+   vector<int> vec1(10,1);
+   vector<int> vec2(7,2);
+   vector<int> vec3(2,3);
+   vector<int> vec4(9,4);
+   vector<int> vec5(4,5);
+
+    brr.push_back(vec1);
+    brr.push_back(vec2);
+    brr.push_back(vec3);
+    brr.push_back(vec4);
+    brr.push_back(vec5);
+
+   for (int i = 0 ; i < brr.size() ; i++)
+    {
+        for (int j = 0 ; j < brr[i].size() ; j++)
+        {
+            cout << brr[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
